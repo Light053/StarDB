347eea9bdd69d4270aa7381ab906eb9af8d59f21
Nicolae <ksirbu519@gmail.com>
2023-11-19 19:06:55 +0200

    Add font Star Jedi

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,20 +5,20 @@ import './header.css';
 const Header = () => {
 	return (
 		<div className="header d-flex">
-			<h3 className='title-header'>
-				<Link to="/">
+			<h3 >
+				<Link to="/" className='title-header'>
 					Star DB
 				</Link>
 			</h3>
 			<ul className="d-flex">
 				<li>
-					<Link to="/people/">People</Link>
+					<Link className='list-header-item' to="/people/">People</Link>
 				</li>
 				<li>
-					<Link to="/planets/">Planets</Link>
+					<Link className='list-header-item' to="/planets/">Planets</Link>
 				</li>
 				<li>
-					<Link to="/starships/">Starships</Link>
+					<Link className='list-header-item' to="/starships/">Starships</Link>
 				</li>
 			</ul>
 		</div>
